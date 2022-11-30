ffc0cfea6853034c4147442ee36b47b7080d92b0
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-11-30 08:08:59 -0600

    added MIDI btn

--- client/src/components/KeyTempoChords/index.js
+++ client/src/components/KeyTempoChords/index.js
@@ -7,19 +7,24 @@ const KeyTempoChords = ({
   selectedProgression,
 }) => {
   return (
-    <div className="icon-data-wrapper">
-      <div className="song-data-container">
-        <img className="data-icon" src={keyIcon}></img>
-        <p className="p1">{selectedKey}</p>
+    <div className="song-data-card">
+      <div className="icon-data-wrapper">
+        <div className="song-data-container">
+          <img className="data-icon" src={keyIcon}></img>
+          <p className="p1">{selectedKey}</p>
+        </div>
+        <div className="song-data-container">
+          <img className="data-icon" src={tempoIcon}></img>
+          <p className="p1">{selectedTempo} BPM</p>
+        </div>
+        <div className="song-data-container">
+          <img className="data-icon" src={chordIcon}></img>
+          <p className="p1">{selectedProgression}</p>
+        </div>
       </div>
-      <div className="song-data-container">
-        <img className="data-icon" src={tempoIcon}></img>
-        <p className="p1">{selectedTempo} BPM</p>
-      </div>
-      <div className="song-data-container">
-        <img className="data-icon" src={chordIcon}></img>
-        <p className="p1">{selectedProgression}</p>
-      </div>
+      <a download href={require(`../../assets/midi/i-bVII-bVI-bVII.mid`)}>
+        <button className="btn btn-tertiary btn-midi">MIDI</button>
+      </a>
     </div>
   );
 };
