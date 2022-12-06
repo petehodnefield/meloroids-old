b377fd9b60c954b71d3bdfcc9456477215375715
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-12-06 08:38:47 -0600

    removed app.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import "../src/assets/styles.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {
