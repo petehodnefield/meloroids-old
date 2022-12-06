6df56df4ae9452bc964d65e03becdcae28145648
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-12-06 08:48:41 -0600

    deploying

--- client/src/index.js
+++ client/src/index.js
@@ -7,8 +7,8 @@ import {
   ApolloClient,
   InMemoryCache,
   createHttpLink,
+  setContext,
 } from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
 
 // Component/Page imports
 import Home from "./pages/Home";
@@ -24,15 +24,15 @@ import HomeScreen from "./pages/HomeScreen";
 
 const httpLink = createHttpLink({ uri: "/graphql" });
 
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem("id_token");
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
-});
+// const authLink = setContext((_, { headers }) => {
+//   const token = localStorage.getItem("id_token");
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : "",
+//     },
+//   };
+// });
 const client = new ApolloClient({
   // link: httpLink,
   uri: "http://localhost:3001/graphql",
